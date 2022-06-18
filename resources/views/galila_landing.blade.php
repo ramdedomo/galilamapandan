3f5e3b04b03b630985ece2301773bebbe3c38bce
unknown <ramdedomo@gmail.com>
2022-06-18 14:54:48 +0800

    modified landing and admin landing carousel

--- resources/views/galila_landing.blade.php
+++ resources/views/galila_landing.blade.php
@@ -97,7 +97,7 @@
                                 style="
                                 
                                 height: 580px;
-                                background: linear-gradient(hsla(0, 0%, 100%, 0.8), hsla(0, 0%, 100%, 0.8)), url(data:image/png;base64,{{ strval($itm->carousel_item_bg) }}) no-repeat;
+                                background: linear-gradient(hsla(0, 0%, 100%, 0.8), hsla(0, 0%, 100%, 0.8)), url(data:image/png;base64,{{ htmlspecialchars($itm->carousel_item_bg) }}) no-repeat;
                                 background-size: cover;
                                 background-position: center;
                                 background-repeat: no-repeat;
