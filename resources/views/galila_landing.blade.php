9dcf15f9f2c101e9c2e6b59a4f7adb63595ebba7
Ryan <ramdedomo@gmail.com>
2022-06-18 20:12:55 +0800

    landing changes

--- resources/views/galila_landing.blade.php
+++ resources/views/galila_landing.blade.php
@@ -151,7 +151,7 @@
 
                                 style="
                                 height: 580px;
-                                background-image: url(@php echo "'"; @endphp data:image/png;base64, {{ ($itm->carousel_item_bg) }}@php echo "'"; @endphp');
+                                background-image: url(@php echo "'"; @endphp data:image/png;base64, {{ ($itm->carousel_item_bg) }}@php echo "'"; @endphp);
                                 background-position: center;
                                 background-size: cover;
                                 background-repeat: no-repeat;
